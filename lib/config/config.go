063c83999bd88b693bb7779b4f8b80999035cc47
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-06-06 18:44:36 +1200

    Function to get the config directory file path

---
+++ lib/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+
+func FindConfigDir() (string, error) {
+	config_dir, _ := os.UserConfigDir()
+	config_dir = fmt.Sprintf("%v/scaf/", config_dir)	
+	_, err := os.ReadDir(config_dir)
+
+	if (!os.IsExist(err)) {
+		return config_dir, nil
+	}
+
+	home_scaf, _ := os.UserHomeDir()
+	home_scaf = fmt.Sprint(home_scaf, "/.scaf/")
+
+	_, err = os.ReadDir(home_scaf)
+
+	if (!os.IsExist(err)) {
+		return home_scaf, nil
+	}
+
+	return "", errors.New("Config files not found")
+}

9fd05ca4f977992e54b57cbd4ab1ae7bb3c3ed89
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2023-06-10 12:40:42 +1200

    Writing test to parse template

--- lib/config/config_test.go
+++ lib/config/config_test.go
@@ -1,9 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"testing"
-	"fmt"
 )
 
 
@@ -25,10 +25,42 @@ func TestFindTemplate(t *testing.T) {
 
 	tmp.Path = file_path
 	tmp.Templates, _ = os.ReadDir(file_path)
-	
+
 	_, err := tmp.FindTemplate("example.toml")
 
 	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestReadTesting(t *testing.T) {
+	var outcome Template = Template{
+		Title: "Go",
+		Description: "Template for quickly setting up Go projects",
+	};
+
+	path, err := FindConfigDir();
+
+	tmp := ConfigDir{}
+	tmp.Path = path;
+	tmp.Templates, _ = os.ReadDir(path)
+
+	template, err := tmp.FindTemplate("example.toml")
+
+	recieved, err := ReadTemplate(template);
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	if outcome.Title != recieved.Title {
+		t.Log("Titles do not match")
+		t.Fail()
+	}
+
+	if outcome.Description != recieved.Description {
+		t.Log("Descriptions do not match")
 		t.Fail()
 	}
 }
